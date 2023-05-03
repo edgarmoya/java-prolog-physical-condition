3db2c5fc7050363ec5263e29184549bd35129ea8
edgarmoya <edgarcmoya@gmail.com>
2023-05-02 21:02:06 -0400

    Initial Commit

---
+++ src/condicionfisica/CondicionFisica.java
@@ -0,0 +1,11 @@
+package condicionfisica;
+
+import condicionfisica.visual.MainWindow;
+
+public class CondicionFisica {
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+        mw.setVisible(true);
+    }
+}
