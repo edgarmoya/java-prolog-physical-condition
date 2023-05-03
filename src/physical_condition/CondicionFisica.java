d1ce7b18965cfbbc7fa3d43b8193e5e030290511
edgarmoya <edgarcmoya@gmail.com>
2023-05-02 22:48:10 -0400

    Add custom components

---
+++ src/physical_condition/CondicionFisica.java
@@ -0,0 +1,11 @@
+package physical_condition;
+
+import physical_condition.visual.MainWindow;
+
+public class CondicionFisica {
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+        mw.setVisible(true);
+    }
+}
---
+++ src/physical_condition/visual/MainWindow.java
@@ -0,0 +1,203 @@
+package physical_condition.visual;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class MainWindow extends javax.swing.JFrame {
+
+    public MainWindow() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        setIconImage(this.getIconImage());
+    }
+    
+    @Override
+    public final Image getIconImage() {
+        Image res = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("physical_condition/images/icon.png"));
+        return res;
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnGroup = new javax.swing.ButtonGroup();
+        bg = new javax.swing.JPanel();
+        btn_test = new physical_condition.customs.Button();
+        btn_questions = new physical_condition.customs.Button();
+        img_left = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        btn_exit = new physical_condition.customs.Button();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Condición Física");
+        setResizable(false);
+
+        bg.setBackground(new java.awt.Color(255, 255, 255));
+
+        btn_test.setBackground(new java.awt.Color(0, 102, 255));
+        btn_test.setForeground(new java.awt.Color(255, 255, 255));
+        btn_test.setIcon(new javax.swing.ImageIcon(getClass().getResource("/physical_condition/images/icon_test.png"))); // NOI18N
+        btn_test.setText("REALIZAR PRUEBA");
+        btn_test.setToolTipText("Realizar diagnóstico para conocer la condición física");
+        btn_test.setFocusPainted(false);
+        btn_test.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_test.setIconTextGap(12);
+        btn_test.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_testActionPerformed(evt);
+            }
+        });
+
+        btn_questions.setBackground(new java.awt.Color(0, 102, 255));
+        btn_questions.setForeground(new java.awt.Color(255, 255, 255));
+        btn_questions.setIcon(new javax.swing.ImageIcon(getClass().getResource("/physical_condition/images/icon_question.png"))); // NOI18N
+        btn_questions.setText("PREGUNTAS FRECUENTES");
+        btn_questions.setToolTipText("Respuesta a preguntas frecuentes");
+        btn_questions.setFocusPainted(false);
+        btn_questions.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_questions.setIconTextGap(12);
+        btn_questions.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_questionsActionPerformed(evt);
+            }
+        });
+
+        img_left.setIcon(new javax.swing.ImageIcon(getClass().getResource("/physical_condition/images/img_background.png"))); // NOI18N
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setText("Prueba de");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel2.setText("Condición Física");
+
+        btn_exit.setBackground(new java.awt.Color(0, 102, 255));
+        btn_exit.setForeground(new java.awt.Color(255, 255, 255));
+        btn_exit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/physical_condition/images/icon_exit.png"))); // NOI18N
+        btn_exit.setText("SALIR");
+        btn_exit.setToolTipText("Salir de la aplicación");
+        btn_exit.setFocusPainted(false);
+        btn_exit.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_exit.setIconTextGap(12);
+        btn_exit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_exitActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
+        bg.setLayout(bgLayout);
+        bgLayout.setHorizontalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgLayout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(img_left)
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(bgLayout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(bgLayout.createSequentialGroup()
+                                .addGap(56, 56, 56)
+                                .addComponent(jLabel1)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgLayout.createSequentialGroup()
+                                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(btn_test, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(btn_questions, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(btn_exit, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(32, 32, 32))))
+                    .addGroup(bgLayout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(jLabel2))))
+        );
+        bgLayout.setVerticalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgLayout.createSequentialGroup()
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(bgLayout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel2)
+                        .addGap(52, 52, 52)
+                        .addComponent(btn_test, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_questions, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_exit, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(img_left))
+                .addGap(0, 0, 0))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_questionsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_questionsActionPerformed
+        System.out.println("questions");
+    }//GEN-LAST:event_btn_questionsActionPerformed
+
+    private void btn_testActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_testActionPerformed
+        TestWindow tw = new TestWindow();
+        tw.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_btn_testActionPerformed
+
+    private void btn_exitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_exitActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btn_exitActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Window".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bg;
+    private javax.swing.ButtonGroup btnGroup;
+    private physical_condition.customs.Button btn_exit;
+    private physical_condition.customs.Button btn_questions;
+    private physical_condition.customs.Button btn_test;
+    private javax.swing.JLabel img_left;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    // End of variables declaration//GEN-END:variables
+}
