f72771cd8ff664e8c4929af899c13cfa50d80968
edgarmoya <edgarcmoya@gmail.com>
2023-05-02 22:57:44 -0400

    Change name to main file

--- src/physical_condition/PhysicalCondition.java
+++ src/physical_condition/PhysicalCondition.java
@@ -2,7 +2,7 @@ package physical_condition;
 
 import physical_condition.visual.MainWindow;
 
-public class CondicionFisica {
+public class PhysicalCondition {
 
     public static void main(String[] args) {
         MainWindow mw = new MainWindow();
